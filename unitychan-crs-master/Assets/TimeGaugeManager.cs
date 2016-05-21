4d60935a33505ab7c0b3c2eeb6b2ebe4ee267401
nunu-e64 <nunu-e64@github.com>
2016-05-21 17:12:18 +0900

    create timegauge

---
+++ unitychan-crs-master/Assets/TimeGaugeManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeGaugeManager : MonoBehaviour {
+
+	private Image timeGauge;
+	private const float timeLimit = 8;
+	private float remainTime;
+	private bool isGaugeMoving;
+
+	// Use this for initialization
+	void Start () {
+		timeGauge = this.GetComponent<Image> ();
+		remainTime = 0.0f;
+		isGaugeMoving = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isGaugeMoving) {
+			remainTime -= Time.deltaTime;
+			if (remainTime <= 0) {
+				StopTimeGauge ();
+			} else {
+				timeGauge.fillAmount = remainTime / timeLimit;
+			}
+		}	
+	}
+
+	public void StartTimeGauge() {
+		timeGauge.fillAmount = 1.0f;
+		remainTime = timeLimit;
+		isGaugeMoving = true;
+	}
+
+	void StopTimeGauge() {
+		timeGauge.fillAmount = 0.0f;
+		isGaugeMoving = false;
+	}
+			
+}
