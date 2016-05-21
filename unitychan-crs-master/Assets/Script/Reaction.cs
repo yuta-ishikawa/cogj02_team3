3206822335c7eadc7dc34675db3e53e44b18e6bc
nunu-e64 <nunu-e64@github.com>
2016-05-21 16:04:52 +0900

    create GameManager and add score management

--- unitychan-crs-master/Assets/Script/Reaction.cs
+++ unitychan-crs-master/Assets/Script/Reaction.cs
@@ -5,31 +5,16 @@ using System.Collections.Generic;
 
 public class Reaction : MonoBehaviour {
 
-	public enum judgementState{
-		MISS,
-		BAD,
-		POOR,
-		GOOD,
-		GREAT,
-		PERFECT
-	}
-
 	[SerializeField]
 	private Sprite[] judgeResultSprites;
 	[SerializeField]
 	private GameObject jugmentResultImage;
 
-	private List<int> scoreList;
-
 	void Start () {
-		scoreList = new List<int> ();
-		for (int i = 0; i <= (int)judgementState.PERFECT; i++) {
-			scoreList.Add (0);
-		}
-		CreateReaction (judgementState.PERFECT, Vector3.zero); //DBEUG
+		CreateReaction (GameManager.JudgementState.PERFECT, Vector3.zero); //DBEUG
 	}
 
-	public void CreateReaction(judgementState judge, Vector3 judgeResultPosition) {
+	public void CreateReaction(GameManager.JudgementState judge, Vector3 judgeResultPosition) {
 		// 五段階評価の表示
 		GameObject judgeResult = GameObject.Instantiate (jugmentResultImage) as GameObject;
 		judgeResult.transform.SetParent (this.transform);
@@ -39,33 +24,28 @@ public class Reaction : MonoBehaviour {
 		// ボイスの再生
 		string voiceName = "";
 		switch (judge){
-		case judgementState.MISS:
+		case GameManager.JudgementState.MISS:
 			voiceName = "SE_sample";
 			break;
-		case judgementState.BAD:
+		case GameManager.JudgementState.BAD:
 			voiceName = "SE_sample";
 			break;
-		case judgementState.POOR:
+		case GameManager.JudgementState.POOR:
 			voiceName = "SE_sample";
 			break;
-		case judgementState.GOOD:
+		case GameManager.JudgementState.GOOD:
 			voiceName = "SE_sample";
 			break;
-		case judgementState.GREAT:
+		case GameManager.JudgementState.GREAT:
 			voiceName = "SE_sample";
 			break;
-		case judgementState.PERFECT:
+		case GameManager.JudgementState.PERFECT:
 			voiceName = "SE_sample";
 			break;
 		}
 		AudioManager.Instance.PlaySE(voiceName);
 
 		// スコア加算
-		scoreList[(int)judge]++;
-		Debug.Log (scoreList [(int)judge]);
-	}
-
-	public List<int> GetScoreList(){
-		return scoreList;
+		GameManager.Instance.AddScore(judge);
 	}
 }
