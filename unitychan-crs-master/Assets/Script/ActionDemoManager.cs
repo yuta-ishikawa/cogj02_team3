ea6dc7809fa19f9552ec29394a144b0b48f1ad12
nunu-e64 <nunu-e64@github.com>
2016-05-22 03:07:32 +0900

    update action manager

--- unitychan-crs-master/Assets/Script/ActionDemoManager.cs
+++ unitychan-crs-master/Assets/Script/ActionDemoManager.cs
@@ -8,7 +8,7 @@ public class ActionDemoManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GameObject.FindObjectOfType<StageManager> ();
+		stageManager = GameObject.FindObjectOfType<StageManager> ();
 	}
 	
 	// Update is called once per frame
