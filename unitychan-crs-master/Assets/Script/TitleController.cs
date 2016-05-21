ac9c15176fc8ede6c70d2e03da094062c0c87473
KousakuKamata <sn13sknkb@yahoo.co.jp>
2016-05-21 14:33:02 +0900

    [追加] ・TitleScene追加 ・仮のScene構成50%ほど完了 ・Startを押すとフェードアウト、完了するとログにおｋが流れる

---
+++ unitychan-crs-master/Assets/Script/TitleController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// TitleSceneに関わる処理を管理するコンポ―ネント
+public class TitleController : MonoBehaviour {
+
+
+	// スタートボタンをクリック
+	public void OnClickStartButton()
+	{
+		ScreenFadeManager.Instance.FadeIn(2.0f, new Color(0.0f, 0.0f, 0.0f),
+			delegate { Debug.Log("Fade Out OK"); });
+	}
+
+}
