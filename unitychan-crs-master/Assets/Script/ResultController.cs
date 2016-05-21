200a110d820a0cb000c6d6b2168a911ef52dfd7a
KousakuKamata <sn13sknkb@yahoo.co.jp>
2016-05-21 19:37:16 +0900

    [変更] リザルトのボイス、テキスト、表情を変更

--- unitychan-crs-master/Assets/Script/ResultController.cs
+++ unitychan-crs-master/Assets/Script/ResultController.cs
@@ -7,10 +7,25 @@ public class ResultController : MonoBehaviour {
 
 	[SerializeField]
 	private FadeParam fadeParam = null;
+	[SerializeField]
+	private ResultVoice resultVoice;
+
+	void Start()
+	{
+		// フェードが終わったらボイス再生
+		ScreenFadeManager.Instance.FadeOut(fadeParam.time, fadeParam.color,
+			delegate {
+				Debug.Log("Fade Out OK");
+				resultVoice.PlayVoice();
+			});
+	}
 
 	// タイトルへの遷移
 	public void OnClickTitleButton()
 	{
+		// フェードアウト中は反応しないように
+		if (ScreenFadeManager.Instance.isFadeAction) return;
+
 		ScreenFadeManager.Instance.FadeIn(fadeParam.time, fadeParam.color,
 			delegate {
 				Debug.Log("Fade In OK");
@@ -21,6 +36,9 @@ public class ResultController : MonoBehaviour {
 	// リトライへの遷移
 	public void OnClickRetryButton()
 	{
+		// フェードアウト中は反応しないように
+		if (ScreenFadeManager.Instance.isFadeAction) return;
+
 		ScreenFadeManager.Instance.FadeIn(fadeParam.time, fadeParam.color,
 			delegate {
 				Debug.Log("Fade In OK");
