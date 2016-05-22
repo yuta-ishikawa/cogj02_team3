7d7ed197df2e3a0dc9b7f0e63b6d7c7abe5f4e11
KousakuKamata <sn13sknkb@yahoo.co.jp>
2016-05-22 16:27:23 +0900

    [作業ナウ]

---
+++ unitychan-crs-master/Assets/Script/ScaleUp.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleUp : MonoBehaviour {
+
+	private enum StateType { Stay, Up, Success };
+
+	[SerializeField]
+	private float addScale = 0.01f;
+	[SerializeField]
+	private StateType stateType = StateType.Stay;
+
+	Vector3 nowScale = new Vector3(0.01f, 0.01f, 0.01f);
+
+	void Awake()
+	{
+		gameObject.transform.localScale = nowScale;
+	}
+
+	public void StartScaleUp()
+	{
+		//stateType
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (stateType != StateType.Up) return;
+
+
+		nowScale += new Vector3(addScale, addScale, addScale);
+
+	}
+}
