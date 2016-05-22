8f33ba81430edb7a3cc01983081111fec5eff70f
nunu-e64 <nunu-e64@github.com>
2016-05-22 10:50:02 +0900

    Rebase branch 'dev-ko-san' on 'origin/develop'

---
+++ unitychan-crs-master/Assets/Script/TensionTableObject.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class TensionTableObject : ScriptableObject {
+	[SerializeField, Tooltip("0～4の順でTooBad～Perfectを設定, 基本的に設定した値以下ならその段階の評価になる")]
+	private List<int> _table;
+	public List<int> table
+	{
+		get { return _table; }
+		private set { _table = value; }
+	}
+}
